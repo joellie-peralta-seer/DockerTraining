204ac90cb28b97232aa155f154698a874a3e9796
Joellie Peralta <joellie.peralta@seer-technologies.com>
2017-12-04 13:51:51 +0800

    add update and delete functionality and change port to 27016

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ class JSONEncoder(json.JSONEncoder):
         return json.JSONEncoder.default(self, o)
 
 
-client = MongoClient('mongodb://backend:27017/dockerdemo')
+client = MongoClient('mongodb://backend:27016/dockerdemo')
 db = client.blogpostDB
 
 app = Flask(__name__)
@@ -24,7 +24,7 @@ app = Flask(__name__)
 def landing_page():
     posts = get_all_posts()
     
-    return render_template('blog.html', posts=json.loads(posts))
+    return render_template('blog.html', posts=json.loads(posts),isEditing=False,isOnMain=True)
 
 
 @app.route('/add_post', methods=['POST'])
@@ -52,6 +52,20 @@ def get_all_posts():
     posts = [post for post in _posts]
     return JSONEncoder().encode(posts)
 
+@app.route("/posts", methods=['PUT'])
+def edit_post(post_id,item_doc):
+    db.blogpostDB.find_one_and_replace({'_id': ObjectId(post_id)}, item_doc)
+    _posts = db.blogpostDB.find()
+    posts = [post for post in _posts]
+    return JSONEncoder().encode(posts)
+
+@app.route("/posts", methods=['DELETE'])
+def delete_post(post_id):
+    db.blogpostDB.delete_one({'_id': ObjectId(post_id)})
+    _posts = db.blogpostDB.find()
+    posts = [post for post in _posts]
+    return JSONEncoder().encode(posts)
+
 
 @app.route('/new', methods=['POST'])
 def new():
@@ -66,10 +80,31 @@ def new():
     posts = [post for post in _posts]
 
     return JSONEncoder().encode(posts[-1])
+@app.route("/delete_this/<post_id>")
+def delete_blog(post_id):
+    delete_post(post_id)
+    return redirect(url_for('landing_page'))
 
 
+@app.route("/post/<post_id>")
+def edit_page(post_id):
+    post = get_one(post_id)
+    return render_template('blog.html', isEditing=True, currentBlog=json.loads(post),isOnMain=False)
+@app.route('/edit/<post_id>', methods=['POST'])
+def edit(post_id):
+    item_doc = {
+        'title': request.form['title'],
+        'post': request.form['post']
+    }
+    edit_post(post_id,item_doc)
+    return redirect(url_for('landing_page'))
+
+    # return JSONEncoder().encode(posts[-1])
 ### Insert function here ###
 
+def get_one(post_id):
+    post = db.blogpostDB.find_one({'_id': ObjectId(post_id)})
+    return JSONEncoder().encode(post)
 
 
 ############################
